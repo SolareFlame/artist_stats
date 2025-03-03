aac1984a6cdef195f478e225a0e7e32581d988b7
SolareFlame <zeldac332@gmail.com>
2025-03-04 00:31:36 +0100

    🚧 Add main entry point (temp: fetch daily data)

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import "reflect-metadata";
+import { AppDataSource } from "./data_source";
+import { SpotifyService } from "./services/SpotifyService";
+
+const main = async () => {
+    console.log(process.env.DB_HOST)
+    try {
+        await AppDataSource.initialize();
+        console.log("Connexion à la base de données établie.");
+
+        const spotifyService = new SpotifyService();
+        await spotifyService.insertDailyMetrics("Ado");
+        console.log("Les métriques journalières ont été insérées avec succès.");
+    } catch (error) {
+        console.error("Erreur lors de l'exécution :", error);
+    } finally {
+        process.exit();
+    }
+};
+
+main();
