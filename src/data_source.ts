9b471c73df8670499b1ce7c2f5dcd22ab8e5c7f5
SolareFlame <zeldac332@gmail.com>
2025-03-04 00:30:51 +0100

    🚀 Add TypeORM data source configuration

---
+++ src/data_source.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import 'dotenv/config';
+import { SpotifyArtistMetrics } from "./entities/SpotifyArtistMetric";
+import { SpotifyTrackMetrics } from "./entities/SpotifyTrackMetric";
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '3306'),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    synchronize: true,
+    logging: false,
+    entities: [
+        SpotifyArtistMetrics,
+        SpotifyTrackMetrics,
+    ],
+});
